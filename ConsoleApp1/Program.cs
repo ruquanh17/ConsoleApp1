cf9abf366a8e0f2f17586fd156cbcf4be2bbe3ee
ruquanh17 <ruquanh@scau.edu.cn>
2017-09-20 16:03:19 +0800

    hw1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("test ");
-            Console.WriteLine("hello world");
+            Console.WriteLine("hello world1");
             Console.ReadLine();
         }
     }
