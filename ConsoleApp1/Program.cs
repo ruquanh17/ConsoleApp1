b12f60bab1571da8488836e0c836310e9dd12658
ruquanh17 <ruquanh@scau.edu.cn>
2017-09-20 16:02:43 +0800

    enter hello world

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("test ");
+            Console.WriteLine("hello world");
             Console.ReadLine();
         }
     }
